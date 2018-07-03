817d25e36e1254f02d9d59b30352eb274939604a
Garrett Van Mourik <gvanmou@s1017129ca.local>
2018-07-03 09:25:07 -0700

    Custom pass now works with the for_loop_tracker

--- LoopAnalysis.h
+++ LoopAnalysis.h
@@ -1,18 +1,16 @@
 #ifndef DEBUG_TYPE 
 #define DEBUG_TYPE "loopAnalysis"
 
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
-#include <map>
+#include <llvm/Pass.h>
+#include <llvm/IR/Function.h>
+#include <llvm/Support/raw_ostream.h>
 
 using namespace llvm;
 
 namespace 
 {
 	struct LoopAnalysis : public FunctionPass {
-		// std::map<std::string, int> opCount;
-		int opCount;
+		int opCount = 0;
 		bool opCodeDNE;
 		static char ID;
 		LoopAnalysis() : FunctionPass(ID) {}
@@ -25,27 +23,20 @@ namespace
 
 		virtual bool runOnFunction(Function &function)
 		{
-			outs() << "Function: " << function.getName() << "()" << "\n";
+			outs() << "------------------------------------\n";
+			outs() << "Loop Analysis Results:\n";
+			outs() << "\tFunction = " << function.getName() << "()" << "\n";
 			// Iterate over basic blocks(BB)
 			for (Function::iterator BB = function.begin(), end = function.end(); BB != end; ++BB)
 			{
 				// Iterate over each instruction(I) in a given BB
 				for (BasicBlock::iterator I = BB->begin(), end = BB->end(); I != end; ++I)
-				{
-					// opCodeDNE = opCount.find(I->getOpcodeName()) == opCount.end();
-					// if ( opCodeDNE )
-					// {
-					// 	opCount[I->getOpcodeName()] = 1;
-					// }
-					// else
-					// {
-					// 	opCount[I->getOpcodeName()] += 1;
-					// }
-					
+				{	
 					opCount++;
 				}
 			}
-			outs() << "opCount = " << opCount << "\n";
+			outs() << "\topCount = " << opCount << "\n";
+			outs() << "------------------------------------\n";
 
 			// opCount.clear();
 			opCount = 0;
@@ -56,6 +47,10 @@ namespace
 
 } //unnamed namespace (internal linkage)
 char LoopAnalysis::ID = 0;
-static RegisterPass<LoopAnalysis>X("loopAnalysis", "Counts the number of operations, reads, and writes per function (should be used on IR of a for-loop)");
+
+FunctionPass *createLoopAnalysisPass() {
+	return new LoopAnalysis();
+}
+
 
 #endif /* DEBUG_TYPE */
