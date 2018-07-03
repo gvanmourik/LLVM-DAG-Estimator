2601da675815b61d43da15ea33add4f559ae0e33
Garrett Van Mourik <gvanmou@s1017129ca.local>
2018-07-03 15:18:37 -0700

    Added analysis features to the pass

--- LoopAnalysisPass.h
+++ LoopAnalysisPass.h
@@ -1,7 +1,6 @@
 #ifndef DEBUG_TYPE 
 #define DEBUG_TYPE "loopAnalysisPass"
 
-// #include <llvm/Pass.h>
 #include <llvm/IR/Function.h>
 #include <llvm/Analysis/LoopInfo.h>
 #include <llvm/Support/raw_ostream.h>
@@ -33,15 +32,13 @@ namespace
 
 			LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
 
-			/// Use the loop info to iterate over each loop
+			/// Use LoopInfo to iterate over each loop and sub-loop
 			for (LoopInfo::iterator iterL=LI.begin(), endLI=LI.end(); iterL != endLI; ++iterL)
 			{
 				emitLoopInfo(*iterL);
 			}
-			// outs() << "\t\tLoop Depth = " << LI.getLoopDepth() << "\n";
 			outs() << "------------------------------------\n";
 
-			// opCount.clear();
 			opCount = 0;
 			return false; //all analysis passes will return false
 		}
@@ -49,8 +46,10 @@ namespace
 	};
 
 } //unnamed namespace (internal linkage)
+
 char LoopAnalysisPass::ID = 0;
 
+/// Required in order to use with the FPM
 FunctionPass *createLoopAnalysisPass() {
 	return new LoopAnalysisPass();
 }
@@ -60,19 +59,42 @@ void emitLoopInfo(Loop *L)
 {
 	unsigned opCount = 0;
 	unsigned bbCount = 0;
+	unsigned readCount = 0;
+	unsigned writeCount = 0;
+
 	for (Loop::block_iterator iterB=L->block_begin(); iterB != L->block_end(); ++iterB)
 	{
 		BasicBlock *BB = *iterB;
 		/// Iterate over each instruction(I) in a given BB
 		for (BasicBlock::iterator iterI = BB->begin(), end = BB->end(); iterI != end; ++iterI)
 		{	
+			Instruction *I = &*iterI;
+			switch ( I->getOpcode() ) {
+				case (Instruction::Load):
+					// outs() << "recording read(" << L->getLoopDepth() << ")...\n";
+					// I->dump();
+					// outs() << "\n";
+					readCount++;
+					break;
+				case (Instruction::Store):
+					// outs() << "recording write(" << L->getLoopDepth() << ")...\n";
+					// I->dump();
+					// outs() << "\n";
+					writeCount++;
+					break;
+				default:
+					break;
+			} 
 			opCount++;
 		}
 		bbCount++;
 	}
-	/// Output opCount for a given loop 
-	outs() << "\t\topCount = " << opCount << " (Loop depth " << L->getLoopDepth() << ")\n";
-	outs() << "\t\tbbCount = " << bbCount << " (Loop depth " << L->getLoopDepth() << ")\n";
+
+	outs() << "\t\t---> Loop Depth " << L->getLoopDepth() << " <---\n";
+	outs() << "\t\topCount = " << opCount << "\n";
+	outs() << "\t\tbbCount = " << bbCount << "\n";
+	outs() << "\t\treadCount = " << readCount << "\n";
+	outs() << "\t\twriteCount = " << writeCount << "\n";
 
 	std::vector<Loop*> subLoops = L->getSubLoops();
 	for (Loop::iterator iterSL=subLoops.begin(), lastSL=subLoops.end(); iterSL != lastSL; ++iterSL)
