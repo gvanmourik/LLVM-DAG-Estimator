ad44480b73f3e7494b5e45d42b9585f610026eb9
Garrett Van Mourik <gvanmou@s1017129ca.local>
2018-07-03 12:04:50 -0700

    Added support for nested for-loops

--- LoopAnalysisPass.h
+++ LoopAnalysisPass.h
@@ -1,11 +1,14 @@
 #ifndef DEBUG_TYPE 
 #define DEBUG_TYPE "loopAnalysisPass"
 
-#include <llvm/Pass.h>
+// #include <llvm/Pass.h>
 #include <llvm/IR/Function.h>
+#include <llvm/Analysis/LoopInfo.h>
 #include <llvm/Support/raw_ostream.h>
 
 using namespace llvm;
+
+void emitLoopInfo(Loop *L);
 
 namespace 
 {
@@ -18,7 +21,8 @@ namespace
 		/// Used to call other passes
 		void getAnalysisUsage(AnalysisUsage &AU) const
 		{
-
+			AU.addRequired<LoopInfoWrapperPass>();
+			AU.setPreservesAll();
 		}
 
 		virtual bool runOnFunction(Function &function)
@@ -26,16 +30,15 @@ namespace
 			outs() << "------------------------------------\n";
 			outs() << "Loop Analysis Results:\n";
 			outs() << "\tFunction = " << function.getName() << "()" << "\n";
-			// Iterate over basic blocks(BB)
-			for (Function::iterator BB = function.begin(), end = function.end(); BB != end; ++BB)
+
+			LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+
+			/// Use the loop info to iterate over each loop
+			for (LoopInfo::iterator iterL=LI.begin(), endLI=LI.end(); iterL != endLI; ++iterL)
 			{
-				// Iterate over each instruction(I) in a given BB
-				for (BasicBlock::iterator I = BB->begin(), end = BB->end(); I != end; ++I)
-				{	
-					opCount++;
-				}
+				emitLoopInfo(*iterL);
 			}
-			outs() << "\topCount = " << opCount << "\n";
+			// outs() << "\t\tLoop Depth = " << LI.getLoopDepth() << "\n";
 			outs() << "------------------------------------\n";
 
 			// opCount.clear();
@@ -50,6 +53,32 @@ char LoopAnalysisPass::ID = 0;
 
 FunctionPass *createLoopAnalysisPass() {
 	return new LoopAnalysisPass();
+}
+
+/// Supports nested loops
+void emitLoopInfo(Loop *L)
+{
+	unsigned opCount = 0;
+	unsigned bbCount = 0;
+	for (Loop::block_iterator iterB=L->block_begin(); iterB != L->block_end(); ++iterB)
+	{
+		BasicBlock *BB = *iterB;
+		/// Iterate over each instruction(I) in a given BB
+		for (BasicBlock::iterator iterI = BB->begin(), end = BB->end(); iterI != end; ++iterI)
+		{	
+			opCount++;
+		}
+		bbCount++;
+	}
+	/// Output opCount for a given loop 
+	outs() << "\t\topCount = " << opCount << " (Loop depth " << L->getLoopDepth() << ")\n";
+	outs() << "\t\tbbCount = " << bbCount << " (Loop depth " << L->getLoopDepth() << ")\n";
+
+	std::vector<Loop*> subLoops = L->getSubLoops();
+	for (Loop::iterator iterSL=subLoops.begin(), lastSL=subLoops.end(); iterSL != lastSL; ++iterSL)
+	{
+		emitLoopInfo(*iterSL);
+	}
 }
 
 
