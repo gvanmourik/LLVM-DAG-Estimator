79dbb533f1ed86bb7b9e31c4e9afdc63244edbe1
Garrett Van Mourik <gvanmou@s1017129ca.local>
2018-07-03 09:32:11 -0700

    Changed the names of the file and pass

---
+++ LoopAnalysisPass.h
@@ -0,0 +1,56 @@
+#ifndef DEBUG_TYPE 
+#define DEBUG_TYPE "loopAnalysisPass"
+
+#include <llvm/Pass.h>
+#include <llvm/IR/Function.h>
+#include <llvm/Support/raw_ostream.h>
+
+using namespace llvm;
+
+namespace 
+{
+	struct LoopAnalysisPass : public FunctionPass {
+		int opCount = 0;
+		bool opCodeDNE;
+		static char ID;
+		LoopAnalysisPass() : FunctionPass(ID) {}
+
+		/// Used to call other passes
+		void getAnalysisUsage(AnalysisUsage &AU) const
+		{
+
+		}
+
+		virtual bool runOnFunction(Function &function)
+		{
+			outs() << "------------------------------------\n";
+			outs() << "Loop Analysis Results:\n";
+			outs() << "\tFunction = " << function.getName() << "()" << "\n";
+			// Iterate over basic blocks(BB)
+			for (Function::iterator BB = function.begin(), end = function.end(); BB != end; ++BB)
+			{
+				// Iterate over each instruction(I) in a given BB
+				for (BasicBlock::iterator I = BB->begin(), end = BB->end(); I != end; ++I)
+				{	
+					opCount++;
+				}
+			}
+			outs() << "\topCount = " << opCount << "\n";
+			outs() << "------------------------------------\n";
+
+			// opCount.clear();
+			opCount = 0;
+			return false; //all analysis passes will return false
+		}
+
+	};
+
+} //unnamed namespace (internal linkage)
+char LoopAnalysisPass::ID = 0;
+
+FunctionPass *createLoopAnalysisPass() {
+	return new LoopAnalysisPass();
+}
+
+
+#endif /* DEBUG_TYPE */
