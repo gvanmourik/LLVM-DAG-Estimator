366fcf036f8d16d2cbd234f80a6697f6c9eb9e7c
Garrett Van Mourik <gvanmou@s1017129ca.local>
2018-07-17 17:04:56 -0700

    Successful in generating a DAG file, however the file does not correctly map the generated IR function. Added a helper script to find, move, and convert the generated DAG file. Combined the legacy/old and new PM versions of LoopInfoAnalysisPass.

---
+++ scripts/loop_tracker_gen_dag.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python
+
+import os
+import sys
+import subprocess
+
+## Process inputs
+iterations = sys.argv[1]
+optLevel = sys.argv[2]
+
+loopTracker = subprocess.Popen("./for_loop_tracker " + iterations + " " + optLevel,
+								# stdout=subprocess.PIPE, #silence IR output
+								stderr=subprocess.PIPE, 
+								shell=True)
+(output, err) = loopTracker.communicate()
+
+## Parse llvm errs() output to find dag file
+startPath = err.find(" '") + 2
+endPath = err.find("'...")
+filePath = err[startPath:endPath]
+
+## Helper variables
+splitIndex = filePath.find("dag.")
+fileDir = filePath[:splitIndex]
+fileName = filePath[splitIndex:]
+fileNamePng = fileName + ".png"
+
+## Produce DAG
+imgDirPath = "images/"
+imgDirExists = os.path.isdir("./" + imgDirPath)
+if imgDirExists is not True:
+	print "Creating image directory..."
+	createImgDir = os.mkdir(imgDirPath)
+
+print "Moving and converting .dot file..."
+copyFile = subprocess.Popen("cp " + filePath + " " + imgDirPath, shell=True)
+removeFile = subprocess.Popen("rm " + filePath, shell=True)
+genDag = subprocess.Popen("dot -T png -O " + imgDirPath + fileName, shell=True)
+genDag.wait()
+
+print "Opening newly created DAG file..."
+openDag = subprocess.Popen("open " + imgDirPath + fileNamePng, shell=True)
+removeDotFile = subprocess.Popen("rm " + imgDirPath + "*.dot", shell=True)
