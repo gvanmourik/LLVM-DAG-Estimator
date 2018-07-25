3a81b99efde65ec352c8b2f54421a819a06016a6
Garrett Van Mourik <gvanmou@s1017129ca.local>
2018-07-25 12:14:27 -0700

    Added a functionName member.

--- include/LoopInfoAnalysis.h
+++ include/LoopInfoAnalysis.h
@@ -11,28 +11,37 @@ class LoopInfoAnalysis : public LoopInfo
 		Loop *L;
 
 	public:
+		std::string functionName;
 		unsigned opCount;
 		unsigned bbCount;
 		unsigned readCount;
 		unsigned writeCount;
+		int width;
+		int depth;
 
 		LoopInfoAnalysis(): 
-			L(nullptr), opCount(0), bbCount(0), readCount(0), writeCount(0) {}
+			L(nullptr), opCount(0), bbCount(0), readCount(0), writeCount(0), 
+			width(0), depth(0) {}
 		LoopInfoAnalysis(Loop *targetLoop): 
-			L(targetLoop), opCount(0), bbCount(0), readCount(0), writeCount(0) {}
+			L(targetLoop), opCount(0), bbCount(0), readCount(0), writeCount(0), 
+			width(0), depth(0) {}
 		~LoopInfoAnalysis(){}
 
 		Loop* getLoop(){ return L; }
 
-		void setLoop(Loop *newLoop){ L=newLoop; }
+		void setLoop(Loop *newLoop) { L=newLoop; }
+		void setFunctionName(std::string name) { functionName = name; }
 
 		void printAnalysis()
 		{
-			outs() << "\t\t---> Loop Depth " << L->getLoopDepth() << " <---\n";
-			outs() << "\t\topCount = " << opCount << "\n";
-			outs() << "\t\tbbCount = " << bbCount << "\n";
-			outs() << "\t\treadCount = " << readCount << "\n";
-			outs() << "\t\twriteCount = " << writeCount << "\n";
+			outs() << "\tFunction: " << functionName << "()\n";
+			outs() << "\t---> Loop Depth " << L->getLoopDepth() << " <---\n";
+			outs() << "\t      opCount = " << opCount << "\n";
+			outs() << "\t      bbCount = " << bbCount << "\n";
+			outs() << "\t    readCount = " << readCount << "\n";
+			outs() << "\t   writeCount = " << writeCount << "\n";
+			outs() << "\t        Width = " << width << "\n";
+			outs() << "\t        Depth = " << depth << "\n";
 		}
 
 		LoopInfoAnalysis& operator=(const LoopInfoAnalysis &LA) 
@@ -41,6 +50,8 @@ class LoopInfoAnalysis : public LoopInfo
 			bbCount = LA.bbCount;
 			readCount = LA.readCount;
 			writeCount = LA.writeCount;
+			width = LA.width;
+			depth = LA.depth;
 
 			return *this;
 		}
