ac956ecbc247113af3fa44e8af9d0ff595b97b81
Garrett Van Mourik <gvanmou@s1017129ca.local>
2018-12-15 13:59:10 -0800

    Commented out the module pass code, until the function is corrected.

--- src/ModuleInfoPass.cpp
+++ src/ModuleInfoPass.cpp
@@ -17,46 +17,46 @@ ModuleInfoPass::gatherAnalysis(llvm::Module &module, ModuleAnalysisInfo &analysi
     /// MERGE loop and function analysis
 
 
-  DAGBuilder *builder = new DAGBuilder();
-  builder->init();
+  // DAGBuilder *builder = new DAGBuilder();
+  // builder->init();
 
-  for (auto fncIter=module.begin(); fncIter!=module.end(); ++fncIter)
-  {
-    llvm::Function *function = &*fncIter;
-    auto &FA = FAM.getResult<FunctionInfoPass>(*function);
-    analysis.FunctionAnalyses[function] = &FA;
+  // for (auto fncIter=module.begin(); fncIter!=module.end(); ++fncIter)
+  // {
+  //   llvm::Function *function = &*fncIter;
+  //   auto &FA = FAM.getResult<FunctionInfoPass>(*function);
+  //   analysis.FunctionAnalyses[function] = &FA;
 
-    for (auto blockIter=function->begin(); blockIter!=function->end(); ++blockIter)
-    {
-      llvm::BasicBlock *BB = &*blockIter;
+  //   for (auto blockIter=function->begin(); blockIter!=function->end(); ++blockIter)
+  //   {
+  //     llvm::BasicBlock *BB = &*blockIter;
 
-      for (auto instIter=BB->begin(); instIter!=BB->end(); ++instIter)
-      {
-        llvm::Instruction *inst = &*instIter;
-        builder->add(inst);
+  //     for (auto instIter=BB->begin(); instIter!=BB->end(); ++instIter)
+  //     {
+  //       llvm::Instruction *inst = &*instIter;
+  //       builder->add(inst);
 
-        switch ( inst->getOpcode() ) {
-        case (llvm::Instruction::Load):
-          analysis.readCount++;
-          break;
-        case (llvm::Instruction::Store):
-          analysis.writeCount++;
-          break;
-        default:
-          break;
-        }
+  //       switch ( inst->getOpcode() ) {
+  //       case (llvm::Instruction::Load):
+  //         analysis.readCount++;
+  //         break;
+  //       case (llvm::Instruction::Store):
+  //         analysis.writeCount++;
+  //         break;
+  //       default:
+  //         break;
+  //       }
 
-        analysis.instCount++;
-      }
-      analysis.bbCount++;
-    }
-  }
+  //       analysis.instCount++;
+  //     }
+  //     analysis.bbCount++;
+  //   }
+  // }
 
-  builder->lock();
-  // builder->fini();
-  llvm::outs() << "ModuleInfoPass...\n";
-  // builder->print();
-  // builder->printDependencyGraph();
-  //analysis.width = builder->getVarWidth();
-  //analysis.depth = builder->getVarDepth();
+  // builder->lock();
+  // // builder->fini();
+  // llvm::outs() << "ModuleInfoPass...\n";
+  // // builder->print();
+  // // builder->printDependencyGraph();
+  // //analysis.width = builder->getVarWidth();
+  // //analysis.depth = builder->getVarDepth();
 }
